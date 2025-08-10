763512700e98a8c4dbc1265b559c9687b39ef938
Utkarsh7707 <sugam7212@gmail.com>
2025-08-10 14:56:22 +0530

    changed get-total-revenue

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,14 +1,7 @@
 import prismadb from "@/lib/prismadb";
-import { Order, OrderItem, Product } from "@prisma/client";
 
-type OrderWithItems = Order & {
-    orderItems: (OrderItem & {
-        product: Product;
-    })[];
-};
-
-export async function getTotalRevenue(storeId: string): Promise<number> {
-    const paidOrders: OrderWithItems[] = await prismadb.order.findMany({
+export async function getTotalRevenue(storeId: string) {
+    const paidOrders = await prismadb.order.findMany({
         where: {
             storeId: storeId,
             isPaid: true
@@ -22,8 +15,8 @@ export async function getTotalRevenue(storeId: string): Promise<number> {
         }
     });
 
-    const totalRevenue: number = paidOrders.reduce((total: number, order: OrderWithItems) => {
-        const orderTotal: number = order.orderItems.reduce((orderSum: number, item) => {
+    const totalRevenue = paidOrders.reduce((total: number, order) => {
+        const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
             return orderSum + item.product.price.toNumber();
         }, 0);
         return total + orderTotal;
